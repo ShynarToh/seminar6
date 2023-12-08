93129ee3b10f9d076dc325c4538f426cad738479
Shynar <shurik_aaa@mail.ru>
2023-12-08 15:56:13 +0600

    string1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// : Задайте двумерный массив символов (тип char 
+//[,]). Создать строку из символов этого массива. a b c => “abcdef” d e f 
+
+namespace String_2 {
+    class Program {
+        static void Main (string [] arg){
+            char [,] chars = new char [,] {{'2', '2','f', 'n'},
+           {'1', '2', 'f', 'v'} };
+            System.Console.WriteLine(CreateString(chars));
+
+        }
+        static string CreateString(char [,] array)  {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+            string result = string.Empty;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result += array[i,j];
+                }
+                
+            }
+            return result;
+        }
+    }
+ }
